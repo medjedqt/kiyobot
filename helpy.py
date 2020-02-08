7c697039171cd159996d3ec09909dbe701f44790
Medjed <58721499+medjedqt@users.noreply.github.com>
2020-02-08 19:14:37 +0800

    More handholding

--- helpy.py
+++ helpy.py
@@ -1,9 +1,11 @@
-hell = """`?kiyo` - poops out kiyo pics
-`?burn` - BURN BURN BURN BURN
-`?step` - NO STEP ON SNEK
-`?upload [insert filename] <insert file>` - Uploads file to a cloud server
-`?download <file name (with extension)>` - Downloads file from cloud server
-`?list` - Lists down every file on the cloud
-`?yt <query>` - Searches up a youtube video
-`?imgur <query>` - Looks up an image on imgur
-`?urban <query>` - Finds an urban definition of a word"""
+hell = """`kiyo` - poops out kiyo pics
+`burn` - BURN BURN BURN BURN
+`step` - NO STEP ON SNEK
+`upload [insert filename] <insert file>` - Uploads file to a cloud server
+`download <file name (with extension)>` - Downloads file from cloud server
+`list` - Lists down every file on the cloud
+`yt <query>` - Searches up a youtube video
+`imgur <query>` - Looks up an image on imgur
+`urban <query>` - Finds an urban definition of a word
+`multi <query>` - Finds multiple images on danbooru
+`dan <query>` - Finds an image on danbooru"""
