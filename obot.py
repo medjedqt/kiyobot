7e817f6bf42af4c5988e0f76da71d7612ec8df46
Medjed <58721499+medjedqt@users.noreply.github.com>
2020-03-25 12:36:06 +0800

    yeeet

--- obot.py
+++ obot.py
@@ -54,7 +54,7 @@ async def on_message(message):
 		if message.author.id == 346316432763650060:
 			e=discord.Embed(color=0xffff00)
 			e.set_image(url="https://cdn.discordapp.com/attachments/569845300244774924/692219666478923776/23a8b2e1-21d4-4dac-84ba-1128207f0e30.png")
-			await ctx.send(embed=e)
+			await message.channel.send(embed=e)
 		else:
 			await message.channel.send(choice(lines))	
 	await bot.process_commands(message)
