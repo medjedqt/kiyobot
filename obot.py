0adb8d38fd6ee1d97db4f3c310203f2f67a05f46
Medjed <58721499+medjedqt@users.noreply.github.com>
2020-03-28 00:12:51 +0800

    Update obot.py

--- obot.py
+++ obot.py
@@ -50,7 +50,7 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_message(message):
-	emoji = '\U000FE0F'
+	emoji = '\\U000FE0F'
 	luck = randint(0,9)
 	if message.author.id == 293395455830654977 and luck >= 2:
 		await message.add_reaction(emoji)
