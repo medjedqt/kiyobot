1e7b0d2d3b4dc56e34bea876294946ddca9adbb8
Firara <62245421+Firara@users.noreply.github.com>
2020-03-28 16:28:22 +0800

    Update obot.py

--- obot.py
+++ obot.py
@@ -109,9 +109,6 @@ async def nwordcount(ctx):
 	await ctx.send(content='stalking')
 	async for message in ctx.channel.history(limit=10000):
 		await message.channel.trigger_typing()
-		await message.edit(content="stalking.")
-		await asyncio.sleep(1)
-		await message.edit(content="stalking..")
 		if message.author.id == ctx.message.author.id:
 			message_text = message.content.upper()
 			if keyword1 in message_text:
