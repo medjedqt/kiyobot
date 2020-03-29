a0563af9e5188f76e8968c72fde49e4d15c2d6b5
Firara <62245421+Firara@users.noreply.github.com>
2020-03-29 17:30:27 +0800

    Update obot.py

--- obot.py
+++ obot.py
@@ -75,7 +75,7 @@ async def on_message(message):
 		else:
 			await message.channel.send(choice(lines))	
 	ch = bot.get_channel(612306757145853953)
-	if message.server is None and message.author != bot.user:
+	if message.guilder is None and message.author != bot.user:
 		await ch.send(content=message.content)
 	await bot.process_commands(message)
 		
