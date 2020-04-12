2ce44de64648dbb31f1c71ba95e8d72b807f6997
Medjed <58721499+medjedqt@users.noreply.github.com>
2020-04-12 10:57:54 +0800

    Update mathe.py

--- mathe.py
+++ mathe.py
@@ -1,2 +1,14 @@
 import math
-calculate = {'sqrt':math.sqrt}
+calculate = {'sqrt':math.sqrt,
+             'factorial':math.factorial,
+             'sin':math.sin,
+             'cos':math.cos,
+             'tan':math.tan,
+             'degrees':math.degrees,
+             'radians':math.radians,
+             'pow':math.pow,
+             'gamma':math.gamma,
+             'pi':math.pi,
+             'e':math.e,
+             'hypot':math.hypot
+            }
