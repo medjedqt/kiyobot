bb6b8d775c6694d672ed960b6d1cdc6c71255e17
Firara <62245421+Firara@users.noreply.github.com>
2020-10-17 20:14:42 +0800

    add get by id

--- epicmodules/kiyodoeschemohgodohfuck.py
+++ epicmodules/kiyodoeschemohgodohfuck.py
@@ -30,6 +30,24 @@ class Chemshit(commands.Cog, name='ChemSpider Database'):
 		e.add_field(name="Molecular Formula:", value=c_formula)
 		e.add_field(name="Average Mass:", value=c_mass)
 		await ctx.send(embed=e)
+		
+	@commands.command(aliases=['compid','cid'],help=hell['CompoundId'])
+	async def CompoundId(self,ctx, idnumber):
+		
+		idnum = idnumber
+		comp = self.cs.get_compound(idnum)
+		c_id = comp.record_id
+		c_name = comp.common_name
+		c_image = comp.image_url
+		c_formula = comp.molecular_formula
+		c_mass = comp.average_mass
+		c_link = f"http://www.chemspider.com/Chemical-Structure.{c_id}.html"
+		e = discord.Embed(title=c_name, url=c_link,color=0x5ec0d1)
+		e.set_image(url=c_image)
+		e.add_field(name="ID:", value=c_id)
+		e.add_field(name="Molecular Formula:", value=c_formula)
+		e.add_field(name="Average Mass:", value=c_mass)
+		await ctx.send(embed=e)
 
 def setup(bot):
 	bot.add_cog(Chemshit(bot))
