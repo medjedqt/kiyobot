b554dd4a3570a56877d666e17048e484e78933d4
Firara <62245421+Firara@users.noreply.github.com>
2020-10-17 19:27:17 +0800

    fixed indent

--- epicmodules/kiyodoeschemohgodohfuck.py
+++ epicmodules/kiyodoeschemohgodohfuck.py
@@ -17,18 +17,18 @@ class Chemshit(commands.Cog, name='ChemSpider Database'):
         searchlist = list()
         for result in cs.search(compound):
             searchlist.append(result)
- 		retop = searchlist[0]
-		c_id = retop.record_id
-		c_name = retop.common_name
-		c_image = retop.image_url
-		c_formula = retop.molecular_formula
-		c_mass = retop.average_mass
-		c_link = f"http://www.chemspider.com/Chemical-Structure.{c_id}.html"
-		e = discord.Embed(title=c_name, url=c_link,color=0x5ec0d1)
-		e.set_image(url=c_image)
-		e.add_field(name="ID:", value=c_id)
-		e.add_field(name="Molecular Formula:", value=c_formula)
-		e.add_field(name="Average Mass:", value=c_mass)
+ 	retop = searchlist[0]
+	c_id = retop.record_id
+	c_name = retop.common_name
+	c_image = retop.image_url
+	c_formula = retop.molecular_formula
+	c_mass = retop.average_mass
+	c_link = f"http://www.chemspider.com/Chemical-Structure.{c_id}.html"
+	e = discord.Embed(title=c_name, url=c_link,color=0x5ec0d1)
+	e.set_image(url=c_image)
+	e.add_field(name="ID:", value=c_id)
+	e.add_field(name="Molecular Formula:", value=c_formula)
+	e.add_field(name="Average Mass:", value=c_mass)
 
 def setup(bot):
 	bot.add_cog(Chemshit(bot))
