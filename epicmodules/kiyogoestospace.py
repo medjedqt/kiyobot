df29aaa4e621a69d878f6db59bcc0cf92e396e39
medjedqt <godtier33@gmail.com>
2020-11-19 20:47:31 +0800

    fixes

--- epicmodules/kiyogoestospace.py
+++ epicmodules/kiyogoestospace.py
@@ -9,8 +9,16 @@ class InterCom(commands.Cog):
 	async def on_message(self, message: discord.Message):
 		if message.author.bot or message.channel.name != "kiyo-intercom" or message.content.startswith("?"):
 			return
+		files = list()
+		if message.attachments == []:
+			files = None
+		else:
+			for atta in message.attachments:
+				await atta.save(atta.filename)
+				files.append(discord.File(atta.filename))
 		for hook in self.bot.commhooks:
-			await hook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.avatar_url, allowed_mentions=discord.AllowedMentions(everyone=False, roles=False))
+			await hook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.avatar_url, allowed_mentions=discord.AllowedMentions(everyone=False, roles=False), files=files)
+		await message.delete()
 
 def setup(bot):
 	bot.add_cog(InterCom(bot))
