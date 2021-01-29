1c18d9f9b02669fe3c122044a5fb367fa77da34d
medjedqt <godtier33@gmail.com>
2021-01-29 20:36:14 +0800

    fucking hell

--- epicmodules/utilititties.py
+++ epicmodules/utilititties.py
@@ -93,8 +93,8 @@ class Utilities(commands.Cog):
 		illu = self.pixapi.illust_detail(art_id)
 		e = discord.Embed(title=illu.title, description=message.content)
 		e.set_author(name=illu['illust']['user']['name'])
-		ext = os.path.splitext(illu.image_urls.large)[1]
-		self.pixapi.download(illu.image_urls.large, name=f"piximg{ext}")
+		ext = os.path.splitext(illu['illust']['image_urls']['large'])[1]
+		self.pixapi.download(illu['illust']['image_urls']['large'], name=f"piximg{ext}")
 		e.set_image(url=f"attachment://piximg{ext}")
 		await message.channel.send(embed=e)
 		await message.delete()
