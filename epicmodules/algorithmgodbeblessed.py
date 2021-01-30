515389713ae112cc3339f3f9e123907457825a40
medjedqt <godtier33@gmail.com>
2021-01-30 10:15:55 +0800

    fucking hell

--- epicmodules/algorithmgodbeblessed.py
+++ epicmodules/algorithmgodbeblessed.py
@@ -14,9 +14,9 @@ from selenium.webdriver.common.keys import Keys
 class MachineLearningShit(commands.Cog, name='Machine Learning Shit'):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
-		self.bot.browser = webdriver.Chrome(ChromeDriverManager().install())
-		self.bot.browser.get('https://www.cleverbot.com')
-		self.bot.browser.execute_script('noteok()')
+		#self.bot.browser = webdriver.Chrome(ChromeDriverManager().install())
+		#self.bot.browser.get('https://www.cleverbot.com')
+		#self.bot.browser.execute_script('noteok()')
 
 	@commands.command()
 	async def word(self, ctx: commands.Context):
