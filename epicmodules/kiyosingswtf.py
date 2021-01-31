bafec67777c0cecd35d614fbcdb5b8ae44ffc37b
medjedqt <godtier33@gmail.com>
2021-01-31 11:00:18 +0800

    eh

---
+++ epicmodules/kiyosingswtf.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+import wavelink
+
+class Music(commands.Cog):
+	def __init__(self, bot: commands.Bot):
+		self.bot = bot
+		if not hasattr(bot, 'wavelink'):
+			self.bot.wavelink = wavelink.Client(bot=self.bot)
+		self.bot.loop.create_task(self.start_nodes())
+	
+	async def start_nodes(self):
+		await self.bot.wait_until_ready()
+		await self.bot.wavelink.initiate_node(host='0.0.0.0', port=2333, rest_uri='http://0.0.0.0:2333', password='youshallnotpass', identifier='TEST', region='singapore')
+	
+	@commands.command()
+	async def connect(self, ctx: commands.Context, *, channel: discord.VoiceChannel=None):
+		if not channel:
+			try:
+				channel = ctx.author.voice.channel
+			except AttributeError:
+				return await ctx.send('No channel to join. Please either specify a valid channel or join one.')
+		player = self.bot.wavelink.get_player(ctx.guild.id)
+		await ctx.send(f'Connecting to **`{channel.name}`**')
+		await player.connect(channel.id)
+
+	@commands.command()
+	async def play(self, ctx, *, query: str):
+		tracks = await self.bot.wavelink.get_tracks(f'ytsearch:{query}')
+		if not tracks:
+			return await ctx.send('Could not find any songs with that query')
+		player = self.bot.wavelink.get_player(ctx.guild.id)
+		if not player.is_connected:
+			await ctx.invoke(self.connect)
+		await ctx.send(f'Added {str(tracks[0])} to the queue')
+		await player.play(tracks[0])
+
+def setup(bot: commands.Bot):
+	bot.add_cog(Music(bot))
