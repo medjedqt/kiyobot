6de2a64958c7dd922a5c83c873009cd77a1eb790
medjedqt <godtier33@gmail.com>
2021-02-04 13:52:29 +0800

    reeee

--- epicmodules/algorithmgodbeblessed.py
+++ epicmodules/algorithmgodbeblessed.py
@@ -18,7 +18,8 @@ class MachineLearningShit(commands.Cog, name='Machine Learning Shit'):
 		chrome = os.environ['GOOGLE_CHROME_SHIM']
 		opts = webdriver.ChromeOptions()
 		opts.binary_location = chrome
-		self.bot.browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=opts)
+		driver = ChromeDriverManager().install()
+		self.bot.browser = webdriver.Chrome(driver, options=opts,)
 		self.bot.browser.get('https://www.cleverbot.com')
 		self.bot.browser.execute_script('noteok()')
 
