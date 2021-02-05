b214b1fa1ae014d1416fcd1037becf70a8b380a6
medjedqt <godtier33@gmail.com>
2021-02-05 15:08:46 +0800

    perm alert!

--- obot.py
+++ obot.py
@@ -131,6 +131,8 @@ async def on_command_error(ctx: commands.Context, error):
 		await ctx.send(content=error, allowed_mentions=discord.AllowedMentions.none())
 	elif isinstance(error, commands.DisabledCommand):
 		await ctx.send(content="Command disabled temporarily")
+	elif isinstance(error, discord.Forbidden):
+		await ctx.send(content="Missing perms!")
 	else:
 		await bot.get_channel(bot.logchan).send(content=error)
 		raise error
