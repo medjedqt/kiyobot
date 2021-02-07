f60133829a960adbba0a16f9190208b31cab3961
medjedqt <godtier33@gmail.com>
2021-02-07 15:01:42 +0800

    hm

--- epicmodules/algorithmgodbeblessed.py
+++ epicmodules/algorithmgodbeblessed.py
@@ -16,7 +16,7 @@ class MachineLearningShit(commands.Cog, name='Machine Learning Shit'):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 		opt = webdriver.FirefoxOptions()
-		opt.add_argument("--headless")
+		opt.add_argument("-headless")
 		self.bot.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_binary="/app/vendor/firefox/firefox")
 		self.bot.browser.get('https://www.cleverbot.com')
 		self.bot.browser.execute_script('noteok()')
