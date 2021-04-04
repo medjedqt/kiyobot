14bebe9414f2332caf2681bbe11b0b225e6880e3
medjedqt <godtier33@gmail.com>
2021-04-04 16:18:41 +0800

    add error handling i guess

--- epicmodules/kiyodoesaslishslash.py
+++ epicmodules/kiyodoesaslishslash.py
@@ -21,12 +21,18 @@ class Slash(commands.Cog):
 	@cog_ext.cog_subcommand(description="googles some image, i guess", base='google', base_description='google tools')
 	async def image(self, ctx: SlashContext, *, query):
 		comm = self.bot.get_cog('Google').image
-		await comm(ctx=ctx, query=query)
+		try:
+			await comm(ctx=ctx, query=query)
+		except AttributeError:
+			await ctx.send(content="Invalid input")
 
 	@cog_ext.cog_slash(description="calculates stuff")
 	async def calculate(self, ctx: SlashContext, *, question):
 		comm = self.bot.get_cog('Google').calculate
-		await comm(ctx=ctx, query=question)
+		try:
+			await comm(ctx=ctx, query=question)
+		except AttributeError:
+			await ctx.send(content="Invalid input")
 	
 	@cog_ext.cog_slash(description="answers your most mundane of questions", base="google")
 	async def answer(self, ctx: SlashContext, *, query):
