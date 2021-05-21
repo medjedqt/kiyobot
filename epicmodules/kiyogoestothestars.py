a11aac9836dfc588b393700b572b1a00ca2c0dfc
medjedqt <godtier33@gmail.com>
2021-05-21 18:32:43 +0800

    kiyo starboard test

---
+++ epicmodules/kiyogoestothestars.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+class Starboard(commands.Cog):
+	async def __init__(self, bot: commands.Bot):
+		self.bot = bot
+		self.cs = bot.cs
+		await self.starboardinit()
+
+	async def starboardinit(self):
+		await self.bot.wait_until_ready()
+		self.stardict = dict()
+		for guild in self.bot.guilds:
+			starchan = discord.utils.get(guild.text_channels, name='kiyo-starboard')
+			if starchan is None:
+				continue
+			self.stardict[str(guild.id)] = starchan
+	
+	@commands.Cog.listener()
+	async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
+		guildid = reaction.message.guild.id
+		starchan: discord.TextChannel = self.stardict.get(str(guildid))
+		if reaction.emoji == '⭐' and reaction.count == 1 and starchan:
+			await starchan.send(content=reaction.message.content)
+
+def setup(bot: commands.Bot):
+	bot.add_cog(Starboard(bot))
+		
