c4edf1dbd7e10b323d3184b9ce83ceadf327ead7
medjedqt <godtier33@gmail.com>
2021-05-21 20:45:58 +0800

    thats a lot

--- epicmodules/kiyogoestothestars.py
+++ epicmodules/kiyogoestothestars.py
@@ -20,23 +20,36 @@ class Starboard(commands.Cog):
 	async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
 		guildid = reaction.message.guild.id
 		starchan: discord.TextChannel = self.stardict.get(str(guildid))
-		if reaction.emoji == '⭐' and reaction.count == 1 and starchan and not user.bot and user != reaction.message.author:
-			lastmsg: discord.Message = await starchan.history(limit=1).flatten()[0]
-			if lastmsg:
-				try:
-					strid = lastmsg.content.split(':')[1].split(',')[0]
-					starid = int(strid)
-				except (ValueError, IndexError):
-					starid = 1
-			else:
-				starid = 1
+		reactors = await reaction.users().flatten()
+		count = 0
+		for reactor in reactors:
+			if not reactor.bot and reactor != reaction.message.author:
+				count += 1
+		if reaction.emoji == '⭐' and count == 4 and starchan:
 			e = discord.Embed()
 			e.color = 0x691337
 			e.set_author(name=str(reaction.message.author), icon_url=reaction.message.author.avatar_url)
-			e.description = reaction.message.content
+			if reaction.message.content:
+				e.description = reaction.message.content
+			if reaction.message.attachments:
+				e.set_image(url=reaction.message.attachments[0].url)
 			e.add_field(name='Original', value=f'[Jump!]({reaction.message.jump_url})')
 			e.timestamp = reaction.message.created_at
-			await starchan.send(content=f'StarID:{starid+1},{reaction.message.channel.mention}',embed=e)
+			await starchan.send(content=f'ID:{reaction.message.id}, {reaction.message.channel.mention}',embed=e)
+
+	@commands.Cog.listener()
+	async def on_reaction_remove(self, reaction: discord.Reaction, user: discord.User):
+		guildid = reaction.message.guild.id
+		starchan: discord.TextChannel = self.stardict.get(str(guildid))
+		reactors = await reaction.users().flatten()
+		count = 0
+		for reactor in reactors:
+			if not reactor.bot and reactor != reaction.message.author:
+				count += 1
+		if reaction.emoji == '⭐' and count < 4:
+			async for message in starchan.history():
+				if message.content.split(':')[1].split(',')[0] == str(reaction.message.id) and message.author.id == self.bot.user.id:
+					return await message.delete()
 
 def setup(bot: commands.Bot):
 	bot.add_cog(Starboard(bot))
