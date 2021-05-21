92532591cd98be4b8bbb7ecb82bcd86ad42cfa09
medjedqt <godtier33@gmail.com>
2021-05-21 19:07:21 +0800

    embed time

--- epicmodules/kiyogoestothestars.py
+++ epicmodules/kiyogoestothestars.py
@@ -21,7 +21,13 @@ class Starboard(commands.Cog):
 		guildid = reaction.message.guild.id
 		starchan: discord.TextChannel = self.stardict.get(str(guildid))
 		if reaction.emoji == '⭐' and reaction.count == 1 and starchan:
-			await starchan.send(content=reaction.message.content)
+			e = discord.Embed()
+			e.color = 0x691337
+			e.set_author(name=str(reaction.message.author), icon_url=reaction.message.author.avatar_url)
+			e.description = reaction.message.content
+			e.add_field(name='Original', value=f'[Jump!]({reaction.message.jump_url})')
+			e.timestamp = reaction.message.created_at
+			await starchan.send(embed=e)
 
 def setup(bot: commands.Bot):
 	bot.add_cog(Starboard(bot))
